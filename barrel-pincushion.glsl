ee393b16203a36754ddc88d726e402052534b9e4
Florian Morel <ayamflow@gmail.com>
2018-07-27 11:29:14 -0700

    Use multiplication instead of pow

--- barrel-pincushion.glsl
+++ barrel-pincushion.glsl
@@ -2,7 +2,7 @@ vec2 barrelPincushion(vec2 uv, float strength) {
     vec2 st = uv - 0.5;
     float theta = atan(st.x, st.y);
     float radius = sqrt(dot(st, st));
-    radius *= 1.0 + strength * pow(radius, 2.0);
+    radius *= 1.0 + strength * (radius * radius);
 
     return 0.5 + radius * vec2(sin(theta), cos(theta));
 }
