27e3ad920983e672fcf1052f8ad758df4f4de104
Florian Morel <ayamflow@gmail.com>
2024-10-28 09:39:04 +0900

    Add trigo optim by tcoppex and bump. Closes #1

--- barrel-pincushion.glsl
+++ barrel-pincushion.glsl
@@ -1,10 +1,7 @@
 vec2 barrelPincushion(vec2 uv, float strength) {
     vec2 st = uv - 0.5;
-    float theta = atan(st.x, st.y);
-    float radius = sqrt(dot(st, st));
-    radius *= 1.0 + strength * (radius * radius);
-
-    return 0.5 + radius * vec2(sin(theta), cos(theta));
+    float radius = 1.0 + strength * dot(st, st);
+    return 0.5 + radius * st;
 }
 
 #pragma glslify: export(barrelPincushion);
